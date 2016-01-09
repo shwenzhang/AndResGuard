421822dd0c34f54ffe3b41d4935fcfe2ab048bf9
simsun <sunsj1231@gmail.com>
2016-01-09 21:34:27 +0800

    change jar name in tool_output

--- tool_output/buildApk.bat
+++ tool_output/buildApk.bat
@@ -1,2 +1,2 @@
-java -jar andresguard-1.1.jar input.apk -config config.xml -out outapk -signature release.keystore testres testres testres
+java -jar AndResGuard-core.jar input.apk -config config.xml -out outapk -signature release.keystore testres testres testres
 pause
