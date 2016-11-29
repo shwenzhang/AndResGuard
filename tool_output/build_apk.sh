3d906e59f1252504cabd67761798033c57f9f9a6
simsun <sunsj1231@gmail.com>
2016-11-29 18:26:21 +0800

    style(build): typo

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar AndResGuard-cli-1.1.15.jar input1.apk -config config.xml -out outapk -signature release.keystore testres testres testres
+java -jar AndResGuard-cli-1.1.15.jar input.apk -config config.xml -out outapk -signature release.keystore testres testres testres
