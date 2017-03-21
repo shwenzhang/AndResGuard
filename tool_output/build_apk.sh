bfac8f59fd9b986eee48cefe7387a3b16a5d1706
simsun <sunsj1231@gmail.com>
2017-03-21 10:45:32 +0800

    feat(cli): upgrade jar verstion to 1.2.0

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar AndResGuard-cli-1.1.16.jar input.apk -config config.xml -out outapk -signature release.keystore testres testres testres
+java -jar AndResGuard-cli-1.2.0.jar input.apk -config config.xml -out outapk -signature release.keystore testres testres testres
