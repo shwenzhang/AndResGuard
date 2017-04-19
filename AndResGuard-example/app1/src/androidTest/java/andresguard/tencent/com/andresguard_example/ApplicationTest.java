b80f80cccda35d2fcec0465536d1532503536c4d
simsun <sunsj1231@gmail.com>
2017-04-19 19:30:14 +0800

    feat(sample): modify sample for reappearing missing resource when we try to build multiple apks in one task

---
+++ AndResGuard-example/app1/src/androidTest/java/andresguard/tencent/com/andresguard_example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package andresguard.tencent.com.andresguard_example;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
