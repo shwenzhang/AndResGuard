2fc76e8c439c8a38059b8ba745c2131b5e568490
Sim Sun <simsun@fb.com>
2018-10-10 00:19:47 -0700

    chore(cli): upgrade jar version to 1.2.15 fix #283

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar AndResGuard-cli-1.2.12.jar input.apk -config config.xml -out outapk -signatureType v2 -signature release.keystore testres testres testres
+java -jar AndResGuard-cli-1.2.15.jar input.apk -config config.xml -out outapk -signatureType v2 -signature release.keystore testres testres testres
